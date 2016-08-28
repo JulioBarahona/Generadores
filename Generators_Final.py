dd1edbb2dbe99b9b3c1a1a97cea669f67e49e981
JulioBarahona <julio.fbm96@gmail.com>
2016-08-28 13:21:26 -0600

    Basic Structrure
    Comments added for EZ understanding

--- Generators_Final.py
+++ Generators_Final.py
@@ -9,3 +9,22 @@ import simpy
 import random
 from math import *
 
+# How many programs the CPU is gonna work
+totalProcesses =  25;
+
+# Generates random memory requeired for a new program, takes away the local memory
+#random 1-10
+def newProgram():
+    return null
+
+# Process thats is run by CPU
+# random 1-10
+def ready():
+    return null
+
+# Can handl eonly 3 instructions
+# Each instructions takes an X time (mutiplier of the memory needed)
+#
+def running():
+    return null
+
