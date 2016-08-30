e1a5249169f8bd65bb3f0ddb924c0d5140359736
DCplayer <cas15151@uvg.edu.gt>
2016-08-29 20:03:33 -0600

    Se arreglo un loop infinito de procesos debido a un typo de intrucciones a instrucciones

--- Generators_Final2.py
+++ Generators_Final2.py
@@ -1,18 +1,19 @@
 # coding=utf-8
-"""
-Julio Barahona , 141206
-Diego Castañeda
-Semana 8
-UVG
-"""
+
+#Julio Barahona , 141206
+#Diego Castaneda
+#Semana 8
+#UVG
+
 import simpy
 import random
 from math import *
 
-class Proceso:
+class SistemaOperativo:
     def __init__(self,env):
         self.cpu = simpy.Resource(env, capacity = 1)
         self.memoria= simpy.Container(env,init=100, capacity = 100)
+
         # How many programs the CPU is gonna work
         totalProcesses = 25;
         contador = 1
@@ -31,7 +32,7 @@ class Proceso:
 
             # Finalmente se crea, con los argumentos anteriores, un nuevo proceso
             contador = contador + 1
-            env.process(self.Simulacion(env, "Proceso %d"contador, inicio, 10 , 2, respuesta))
+            env.process(self.Simulacion(env, "Proceso %d"%contador, inicio, mem , instrucciones, respuesta))
 
 
 
@@ -40,12 +41,12 @@ class Proceso:
         yield env.timeout(inicio)
         print('%s inicio a procesar en tiempo %d'%(nombre, env.now))
 
-        yield simular.memoria.get(mem)
+        yield self.memoria.get(mem)
 
         while instrucciones > 0:
-            with simular.cpu.request() as req:
+            with self.cpu.request() as req:
                 yield req
-                intrucciones = instrucciones - 3
+                instrucciones = instrucciones - 3
 
                 if respuesta == 1:
                     print ("Proceso %s entra a IO en tiempo %d"%(nombre,env.now))
@@ -58,21 +59,10 @@ class Proceso:
 
 # Elementos de Simpy antes de comenzar a trabaajar
 env = simpy.Environment()
-memoria1 = simpy.Container(env, init = 100, capacity = 100)
-cpu     = simpy.Resource(env, capacity = 1)
-IO     = simpy.Resource(env, capacity = 1)
-
 
 #*******************************
-simular = Proceso(env)
+simular = SistemaOperativo(env)
 #*******************************
-
-
-
-#Datos que se necesitan para cada proceso
-
-RANDOM_SEED = 42            # ESTO EN REALIDAD NO SIRVE, HAY QUE CHEQUEAR
-random.seed(RANDOM_SEED)    #EL EJEMPLO DE DOUGLAS Y COMO LO USO
 
 
 #iniciar hasta que se acaben los procesos
